c874c7e5327ba7839f828f3dc2c0a761b6a1cbe5
LokmanHossen <lokmanhossen230@gmail.com>
2026-01-22 11:43:43 +0600

    Yesterday, today data

--- lib/core/constants/icon_path.dart
+++ lib/core/constants/icon_path.dart
@@ -6,5 +6,11 @@ class IconPath {
   static const String sunnyIcon = '$_basePath/sunny_icon.png';
   static const String moonIcon = '$_basePath/moon_icon.png';
 
-
+  static const String acIcon = '$_basePath/ac.png';
+  static const String gererationIcon = '$_basePath/generation.png';
+  static const String liveIcon = '$_basePath/live.png';
+  static const String calculativeIcon = '$_basePath/calculative.png';
+  static const String returnIcon = '$_basePath/return.png';
+  static const String energyIcon = '$_basePath/energy.png';
+  static const String solarIcon = '$_basePath/solar.png';
 }
--- lib/feature/dashboard_view/screens/dashboard_one_screen.dart
+++ lib/feature/dashboard_view/screens/dashboard_one_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:scube_app/core/constants/app_colors.dart';
+import 'package:scube_app/core/constants/icon_path.dart';
 import 'package:scube_app/feature/dashboard_view/controller/temperature_card_controller.dart';
 import 'package:scube_app/feature/dashboard_view/controller/dashboard_one_controller.dart';
 import 'package:scube_app/feature/dashboard_view/model/dashboard_one_model.dart';
 import 'package:scube_app/feature/dashboard_view/screens/temperature_card.dart';
-
 
 class DashboardOneScreen extends StatelessWidget {
   const DashboardOneScreen({super.key});
@@ -13,7 +13,9 @@ class DashboardOneScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final DashboardOneController controller = Get.put(DashboardOneController());
-    final TemperatureController temperatureController = Get.put(TemperatureController());
+    final TemperatureController temperatureController = Get.put(
+      TemperatureController(),
+    );
 
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
@@ -32,7 +34,10 @@ class DashboardOneScreen extends StatelessWidget {
           Stack(
             children: [
               IconButton(
-                icon: const Icon(Icons.notifications_outlined, color: Colors.black),
+                icon: const Icon(
+                  Icons.notifications_outlined,
+                  color: Colors.black,
+                ),
                 onPressed: () {},
               ),
               Positioned(
@@ -51,38 +56,43 @@ class DashboardOneScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Obx(() => SingleChildScrollView(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            _buildNavigateButton(controller),
-            const SizedBox(height: 16),
-            _buildTopMetrics(controller.solarData.value),
-            const SizedBox(height: 16),
-            Obx(
-              () => TemperatureCard(
-                temperature: temperatureController.temperature.value,
-                windSpeed: temperatureController.windSpeed.value,
-                windDirection: temperatureController.windDirection.value,
-                irradiation: temperatureController.irradiation.value,
-                weatherIcon: temperatureController.weatherIcon.value,
-                temperatureColor: temperatureController.temperatureColor.value,
-              ),
+      body: Obx(
+        () => SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          child: SafeArea(
+            child: Column(
+              children: [
+                _buildNavigateButton(controller),
+                const SizedBox(height: 16),
+                _buildTopMetrics(controller.solarData.value),
+                const SizedBox(height: 16),
+                Obx(
+                  () => TemperatureCard(
+                    temperature: temperatureController.temperature.value,
+                    windSpeed: temperatureController.windSpeed.value,
+                    windDirection: temperatureController.windDirection.value,
+                    irradiation: temperatureController.irradiation.value,
+                    weatherIcon: temperatureController.weatherIcon.value,
+                    temperatureColor:
+                        temperatureController.temperatureColor.value,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                _buildYesterdayDataContainer(controller.solarData.value),
+                const SizedBox(height: 16),
+                _buildSystemInfo(controller.solarData.value),
+                const SizedBox(height: 16),
+                ...controller.solarData.value.inverters.map(
+                  (inv) => Padding(
+                    padding: const EdgeInsets.only(bottom: 16),
+                    child: _buildInverterCard(inv),
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 16),
-            _buildYesterdayDataContainer(controller.solarData.value),
-            const SizedBox(height: 16),
-            _buildSystemInfo(controller.solarData.value),
-            const SizedBox(height: 16),
-            ...controller.solarData.value.inverters
-                .map((inv) => Padding(
-                      padding: const EdgeInsets.only(bottom: 16),
-                      child: _buildInverterCard(inv),
-                    ))
-                ,
-          ],
+          ),
         ),
-      )),
+      ),
     );
   }
 
@@ -112,10 +122,14 @@ class DashboardOneScreen extends StatelessWidget {
             Container(
               padding: const EdgeInsets.all(2),
               decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.3),
+                color: Colors.white.withValues(alpha: 0.3),
                 shape: BoxShape.circle,
               ),
-              child: const Icon(Icons.arrow_forward_ios, color: Colors.white, size: 16),
+              child: const Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.white,
+                size: 16,
+              ),
             ),
           ],
         ),
@@ -130,9 +144,9 @@ class DashboardOneScreen extends StatelessWidget {
           children: [
             Expanded(
               child: MetricCard(
-                icon: Icons.flash_on_rounded,
-                iconColor: const Color(0xFF4CAF50),
-                iconBgColor: const Color(0xFFE8F5E9),
+                imagePath: IconPath.acIcon,
+
+                iconBgColor: const Color(0xFFE9FAE9),
                 title: '${data.liveAcPower.toInt()} kW',
                 subtitle: 'Live AC Power',
               ),
@@ -140,9 +154,9 @@ class DashboardOneScreen extends StatelessWidget {
             const SizedBox(width: 12),
             Expanded(
               child: MetricCard(
-                icon: Icons.factory_outlined,
-                iconColor: const Color(0xFF2196F3),
-                iconBgColor: const Color(0xFFE3F2FD),
+                imagePath: IconPath.gererationIcon,
+
+                iconBgColor: const Color(0xFFE7F8F9),
                 title: '${data.plantGeneration} %',
                 subtitle: 'Plant Generation',
               ),
@@ -150,9 +164,9 @@ class DashboardOneScreen extends StatelessWidget {
             const SizedBox(width: 12),
             Expanded(
               child: MetricCard(
-                icon: Icons.person_outline,
-                iconColor: const Color(0xFF9C27B0),
-                iconBgColor: const Color(0xFFF3E5F5),
+                imagePath: IconPath.liveIcon,
+
+                iconBgColor: const Color(0xFFF2F2FB),
                 title: '${data.livePr} %',
                 subtitle: 'Live PR',
               ),
@@ -164,9 +178,9 @@ class DashboardOneScreen extends StatelessWidget {
           children: [
             Expanded(
               child: MetricCard(
-                icon: Icons.show_chart,
-                iconColor: const Color(0xFF2196F3),
-                iconBgColor: const Color(0xFFE3F2FD),
+                imagePath: IconPath.calculativeIcon,
+
+                iconBgColor: const Color(0xFFE4F2FF),
                 title: '${data.cumulativePr} %',
                 subtitle: 'Cumulative PR',
               ),
@@ -174,20 +188,22 @@ class DashboardOneScreen extends StatelessWidget {
             const SizedBox(width: 12),
             Expanded(
               child: MetricCard(
-                icon: Icons.currency_exchange,
-                iconColor: const Color(0xFFFF9800),
-                iconBgColor: const Color(0xFFFFF3E0),
+                imagePath: IconPath.returnIcon,
+
+                iconBgColor: const Color(0xFFFDF6EA),
                 title: '${data.returnPv.toInt()} ৳',
-                subtitle: 'Return PV(Till Today)',
+                subtitle: 'Return PV',
+                subSmalltitle: '(Till Today)',
+
                 smallSubtitle: true,
               ),
             ),
             const SizedBox(width: 12),
             Expanded(
               child: MetricCard(
-                icon: Icons.energy_savings_leaf_outlined,
-                iconColor: const Color(0xFF9C27B0),
-                iconBgColor: const Color(0xFFF3E5F5),
+                imagePath: IconPath.energyIcon,
+
+                iconBgColor: const Color(0xFFF7E8FD),
                 title: '${data.totalEnergy.toInt()} kWh',
                 subtitle: 'Total Energy',
               ),
@@ -231,44 +247,47 @@ class DashboardOneScreen extends StatelessWidget {
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(16),
-      
       ),
       child: Column(
         children: [
           Padding(
             padding: const EdgeInsets.fromLTRB(20, 20, 20, 12),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
+                Expanded(child: SizedBox()),
                 Expanded(
                   child: Text(
                     "Yesterday's Data",
                     style: TextStyle(
                       fontWeight: FontWeight.w500,
-                      fontSize: 16,
+                      fontSize: 12,
                       color: AppColors.textPrimaryColor,
                     ),
                   ),
                 ),
-                Expanded(
-                  child: Text(
-                    "Today's Data",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 16,
-                      color: AppColors.textPrimaryColor
-                    ),
+                SizedBox(width: 10),
+                Text(
+                  "Today's Data",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 12,
+                    color: AppColors.textPrimaryColor,
                   ),
                 ),
               ],
             ),
           ),
-          Divider(height: 1, thickness: 1, color: Colors.grey[300]),
+          Divider(height: 1, thickness: 1, color: Color(0xFF9AA9BC)),
           Column(
             children: List.generate(
               comparisonData.length,
               (index) => Container(
                 color: index.isEven ? Color(0xFFEEF3F9) : Colors.white,
-                padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+                padding: const EdgeInsets.symmetric(
+                  vertical: 16,
+                  horizontal: 20,
+                ),
                 child: _buildComparisonRow(
                   comparisonData[index]['label']!,
                   comparisonData[index]['yesterday']!,
@@ -284,6 +303,7 @@ class DashboardOneScreen extends StatelessWidget {
 
   Widget _buildComparisonRow(String label, String yesterday, String today) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Expanded(
           flex: 5,
@@ -291,7 +311,7 @@ class DashboardOneScreen extends StatelessWidget {
             label,
             style: const TextStyle(
               fontWeight: FontWeight.w400,
-              fontSize: 15,
+              fontSize: 12,
               color: AppColors.textPrimaryColor,
             ),
           ),
@@ -301,21 +321,19 @@ class DashboardOneScreen extends StatelessWidget {
           child: Text(
             yesterday,
             style: const TextStyle(
-              fontSize: 15,
+              fontSize: 12,
               fontWeight: FontWeight.w600,
-              color: AppColors.textPrimaryColor
+              color: AppColors.textPrimaryColor,
             ),
           ),
         ),
-        Expanded(
-          flex: 4,
-          child: Text(
-            today,
-            style: const TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.w600,
-              color: AppColors.textPrimaryColor
-            ),
+        SizedBox(width: 10),
+        Text(
+          today,
+          style: const TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w600,
+            color: AppColors.textPrimaryColor,
           ),
         ),
       ],
@@ -334,30 +352,27 @@ class DashboardOneScreen extends StatelessWidget {
           child: Row(
             children: [
               Container(
-                padding: const EdgeInsets.all(12),
+                padding: const EdgeInsets.all(4),
                 decoration: BoxDecoration(
-                  color: const Color(0xFFE3F2FD),
-                  borderRadius: BorderRadius.circular(12),
+                  color: const Color(0xFFE4F2FF),
+                  borderRadius: BorderRadius.circular(6),
                 ),
-                child: const Icon(
-                  Icons.solar_power_outlined,
-                  color: Color(0xFF2196F3),
-                  size: 28,
-                ),
+                child: Image.asset(IconPath.solarIcon, width: 20, height: 20),
               ),
-              const SizedBox(width: 12),
+              const SizedBox(width: 4),
               Expanded(
                 child: RichText(
                   text: TextSpan(
-                    style: const TextStyle(
-                      fontSize: 15,
+                    style: TextStyle(
+                      fontSize: 12,
                       color: AppColors.textPrimaryColor,
                       fontWeight: FontWeight.w400,
                     ),
                     children: [
                       const TextSpan(text: 'Total Num of PV Module   :   '),
                       TextSpan(
-                        text: '${data.totalPvModules} pcs. (${data.pcsPerModule} Wp each)',
+                        text:
+                            '${data.totalPvModules} pcs. (${data.pcsPerModule} Wp each)',
                         style: const TextStyle(fontWeight: FontWeight.w600),
                       ),
                     ],
@@ -372,7 +387,7 @@ class DashboardOneScreen extends StatelessWidget {
           children: [
             Expanded(
               child: InfoCard(
-                icon: Icons.electric_bolt_outlined,
+                imagePath: IconPath.solarIcon,
                 iconColor: const Color(0xFF2196F3),
                 iconBgColor: const Color(0xFFE3F2FD),
                 title: 'Total AC Capacity',
@@ -382,7 +397,7 @@ class DashboardOneScreen extends StatelessWidget {
             const SizedBox(width: 12),
             Expanded(
               child: InfoCard(
-                icon: Icons.cable_outlined,
+                imagePath: IconPath.solarIcon,
                 iconColor: const Color(0xFF2196F3),
                 iconBgColor: const Color(0xFFE3F2FD),
                 title: 'Total DC Capacity',
@@ -545,59 +560,81 @@ class DashboardOneScreen extends StatelessWidget {
 }
 
 class MetricCard extends StatelessWidget {
-  final IconData icon;
-  final Color iconColor;
+  final String imagePath;
+  // final Color iconColor;
   final Color iconBgColor;
   final String title;
   final String subtitle;
+  final String? subSmalltitle;
+
   final bool smallSubtitle;
 
   const MetricCard({
     super.key,
-    required this.icon,
-    required this.iconColor,
+    required this.imagePath,
+    // required this.iconColor,
     required this.iconBgColor,
     required this.title,
     required this.subtitle,
+    this.subSmalltitle,
+
     this.smallSubtitle = false,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(14),
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(12),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: Row(
         children: [
           Container(
-            padding: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(4),
             decoration: BoxDecoration(
               color: iconBgColor,
-              borderRadius: BorderRadius.circular(10),
+              borderRadius: BorderRadius.circular(6),
             ),
-            child: Icon(icon, color: iconColor, size: 24),
+            child: Image.asset(imagePath, width: 12, height: 12),
+
+            //Icon(imagePath, color: iconColor, size: 10),
           ),
-          const SizedBox(height: 12),
-          Text(
-            title,
-            style: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w700,
-              color: AppColors.textPrimaryColor,
-            ),
-          ),
-          const SizedBox(height: 4),
-          Text(
-            subtitle,
-            style: TextStyle(
-              fontSize: smallSubtitle ? 10.5 : 12,
-              color: Colors.grey[600],
-              fontWeight: FontWeight.w400,
-            ),
+          const SizedBox(width: 4),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                  color: AppColors.textPrimaryColor,
+                ),
+              ),
+              const SizedBox(height: 4),
+              RichText(
+                text: TextSpan(
+                  text: subtitle,
+                  style: TextStyle(
+                    fontSize: 9,
+                    color: Color(0xFF5E5E5E),
+                    fontWeight: FontWeight.w400,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: subSmalltitle,
+                      style: TextStyle(
+                        fontSize: 6,
+                        color: Color(0xFF5E5E5E),
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ],
       ),
@@ -606,7 +643,7 @@ class MetricCard extends StatelessWidget {
 }
 
 class InfoCard extends StatelessWidget {
-  final IconData icon;
+  final String imagePath;
   final Color iconColor;
   final Color iconBgColor;
   final String title;
@@ -614,7 +651,7 @@ class InfoCard extends StatelessWidget {
 
   const InfoCard({
     super.key,
-    required this.icon,
+    required this.imagePath,
     required this.iconColor,
     required this.iconBgColor,
     required this.title,
@@ -637,7 +674,9 @@ class InfoCard extends StatelessWidget {
               color: iconBgColor,
               borderRadius: BorderRadius.circular(10),
             ),
-            child: Icon(icon, color: iconColor, size: 24),
+            child: Image.asset(imagePath, width: 12, height: 12),
+
+            //Icon(icon, color: iconColor, size: 24),
           ),
           const SizedBox(width: 12),
           Expanded(
@@ -647,7 +686,7 @@ class InfoCard extends StatelessWidget {
                 Text(
                   title,
                   style: TextStyle(
-                    fontSize: 12,
+                    fontSize: 10,
                     color: Colors.grey[600],
                     fontWeight: FontWeight.w400,
                   ),
@@ -656,7 +695,7 @@ class InfoCard extends StatelessWidget {
                 Text(
                   value,
                   style: const TextStyle(
-                    fontSize: 15,
+                    fontSize: 10,
                     fontWeight: FontWeight.w700,
                     color: AppColors.textPrimaryColor,
                   ),
